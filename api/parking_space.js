2492a6aaa0e4658ec9f3e2a6fdad70cbd45d6529
Dylan Villaruel <thraxxdv@gmail.com>
2022-07-20 07:37:52 +0800

    build: api request for parking vehicle

--- api/parking_space.js
+++ api/parking_space.js
@@ -7,3 +7,12 @@ export const getParkingSpaces = () : Promise<Object> => {
     return axios.get(`${API_URL}/v1/parking/spaces`, {
     });
 }
+
+export const parkVehicle = (gate: number, type: number, timestamp: string, uuid: string = '') : Promise<Object> => {
+    return axios.post(`${API_URL}/v1/parking/spaces`, {
+        gate: gate,
+        vehicle_type_id: type,
+        timestamp: timestamp,
+        uuid: uuid
+    });
+}
