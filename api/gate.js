870bfd0a51e4e188aafba41b109c2faba644edb0
Dylan Villaruel <thraxxdv@gmail.com>
2022-07-20 21:13:38 +0800

    build: added add and delete gate api calls

--- api/gate.js
+++ api/gate.js
@@ -6,3 +6,15 @@ import axios from "axios";
 export const getGates = (): Promise<Object> => {
   return axios.get(`${API_URL}/v1/gate/get`);
 };
+
+export const createGate = (nearestSpace: number): Promise<Object> => {
+  return axios.post(`${API_URL}/v1/gate/create`, {
+    nearest_space: nearestSpace,
+  });
+};
+
+export const deleteGate = (nearestSpace: string): Promise<Object> => {
+  return axios.delete(`${API_URL}/v1/gate/delete`, {
+    nearest_space: nearestSpace,
+  });
+};
