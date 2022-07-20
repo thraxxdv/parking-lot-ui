cc33c42a449a9a1311dad8d32f66e76ec1af332d
Dylan Villaruel <thraxxdv@gmail.com>
2022-07-20 21:31:22 +0800

    build: changed deleteGate api call parameters structure and implemented functionality into AddRemoveGate.js

--- api/gate.js
+++ api/gate.js
@@ -7,14 +7,16 @@ export const getGates = (): Promise<Object> => {
   return axios.get(`${API_URL}/v1/gate/get`);
 };
 
-export const createGate = (nearestSpace: number): Promise<Object> => {
+export const createGate = (nearestSpace: number | string): Promise<Object> => {
   return axios.post(`${API_URL}/v1/gate/create`, {
     nearest_space: nearestSpace,
   });
 };
 
-export const deleteGate = (nearestSpace: string): Promise<Object> => {
+export const deleteGate = (nearestSpace: number | string): Promise<Object> => {
   return axios.delete(`${API_URL}/v1/gate/delete`, {
-    nearest_space: nearestSpace,
+    data: {
+      nearest_space: nearestSpace,
+    },
   });
 };
--- components/forms/AddRemoveGate.js
+++ components/forms/AddRemoveGate.js
@@ -3,10 +3,24 @@ import type { Element } from "react";
 
 import React from "react";
 import { useState } from "react";
-import { ToastContainer } from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
+import { createGate, deleteGate } from "../../api/gate";
 
 function AddRemoveGate(): Element<"form"> {
-  const [gate, setGate] = useState("");
+  const [nearestSpace, setNearestSpace] = useState("");
+
+  const handlePress = (isDelete = true) => {
+    let call = isDelete ? deleteGate(nearestSpace) : createGate(nearestSpace);
+
+    call
+      .then((r) => {
+        let msg = isDelete ? "Gate deleted" : "Gate added";
+        toast(msg, {
+          type: isDelete ? "info" : "success",
+        });
+      })
+      .catch((e) => console.log(e));
+  };
 
   return (
     <form className="d-flex my-1 bg-gray-500 p-2 flex-grow-1 me-3">
@@ -14,20 +28,25 @@ function AddRemoveGate(): Element<"form"> {
       <input
         type="text"
         className="form-control me-3 flex-grow-1"
-        id="gate"
-        name="gate"
-        value={gate}
-        onChange={(e) => setGate(e.target.value)}
+        id="nearest-space"
+        name="nearest-space"
+        value={nearestSpace}
+        onChange={(e) => setNearestSpace(e.target.value)}
         placeholder="Place Gate Near"
       />
 
       <button
         className="btn btn-primary text-white fw-bold me-3 w-50"
         type="button"
+        onClick={() => handlePress(false)}
       >
         Add Gate
       </button>
-      <button className="btn btn-red text-white fw-bold w-50" type="button">
+      <button
+        className="btn btn-red text-white fw-bold w-50"
+        type="button"
+        onClick={() => handlePress()}
+      >
         Remove Gate
       </button>
     </form>
