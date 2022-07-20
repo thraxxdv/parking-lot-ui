60da5de584c28414dfaa7a7d2918853eb4e800d2
Dylan Villaruel <thraxxdv@gmail.com>
2022-07-20 08:23:51 +0800

    build: update api constants file

--- api/constants.js
+++ api/constants.js
@@ -1,2 +1,3 @@
 // @flow
-export const API_URL: string = process.env.NEXT_PUBLIC_API_URL ? process.env.NEXT_PUBLIC_API_URL : '';
+export const API_URL: string = process.env.NEXT_PUBLIC_API_URL ? process.env.NEXT_PUBLIC_API_URL : 'http://127.0.0.1:8000/api';
+export const API_BASE_URL: string = process.env.NEXT_PUBLIC_API_BASE_URL ? process.env.NEXT_PUBLIC_API_BASE_URL : 'http://127.0.0.1:8000';
