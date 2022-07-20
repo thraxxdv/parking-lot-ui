c6034cc70c1e08d9a509ab543e15c55eca3be613
Dylan Villaruel <thraxxdv@gmail.com>
2022-07-20 09:04:00 +0800

    style: removed empty bracker from getParkingSpaces api call

--- api/parking_space.js
+++ api/parking_space.js
@@ -4,8 +4,7 @@ import { API_URL } from "./constants";
 import axios from "axios"
 
 export const getParkingSpaces = () : Promise<Object> => {
-    return axios.get(`${API_URL}/v1/parking/spaces`, {
-    });
+    return axios.get(`${API_URL}/v1/parking/spaces`);
 }
 
 export const parkVehicle = (gate: number, type: number, timestamp: string, uuid: string = '') : Promise<Object> => {
