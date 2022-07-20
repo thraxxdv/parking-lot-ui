1c56a86bc539b69c530a19b205a9e69b1b7e3ee0
Dylan Villaruel <thraxxdv@gmail.com>
2022-07-20 16:03:56 +0800

    build: unparking functionality and api call

--- api/parking_space.js
+++ api/parking_space.js
@@ -1,17 +1,32 @@
 // @flow
 import { API_URL } from "./constants";
 
-import axios from "axios"
+import axios from "axios";
 
-export const getParkingSpaces = () : Promise<Object> => {
-    return axios.get(`${API_URL}/v1/parking/spaces`);
-}
+export const getParkingSpaces = (): Promise<Object> => {
+  return axios.get(`${API_URL}/v1/parking/spaces`);
+};
 
-export const parkVehicle = (gate: number, type: number, timestamp: string, uuid: string = '') : Promise<Object> => {
-    return axios.put(`${API_URL}/v1/parking/park`, {
-        gate: gate,
-        vehicle_type_id: type,
-        timestamp: timestamp,
-        uuid: uuid
-    });
-}
+export const parkVehicle = (
+  gate: number,
+  type: number,
+  timestamp: string,
+  uuid: string = ""
+): Promise<Object> => {
+  return axios.put(`${API_URL}/v1/parking/park`, {
+    gate: gate,
+    vehicle_type_id: type,
+    timestamp: timestamp,
+    uuid: uuid,
+  });
+};
+
+export const unparkVehicle = (
+  timestamp: string,
+  uuid: string
+): Promise<Object> => {
+  return axios.put(`${API_URL}/v1/parking/unpark`, {
+    timestamp: timestamp,
+    uuid: uuid,
+  });
+};
--- components/forms/VehicleUnparking.js
+++ components/forms/VehicleUnparking.js
@@ -1,15 +1,43 @@
 // @flow
 import type { Element } from "react";
 
+import { unparkVehicle } from "../../api/parking_space";
+
 import React from "react";
 import { useState } from "react";
-import { ToastContainer } from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
+import LaravelValidationParser from "../../utils/LaravelValidationParser";
+import HttpErrorHandler from "../../utils/HttpErrorHandler";
 
 function VehicleUnparking(): Element<"form"> {
   const [vehicleId, setVehicleId] = useState("");
   const [timestamp, setTimestamp] = useState("");
+
+  const handleUnpark = (e) => {
+    e.preventDefault();
+    unparkVehicle(timestamp, vehicleId)
+      .then((r) => {
+        let { parked_on, left_on, fee } = r.data;
+        toast(
+          `${vehicleId} has been unparked. Duration ${parked_on} - ${left_on} with ₱${fee} fee.`,
+          {
+            type: "info",
+            autoClose: false,
+          }
+        );
+      })
+      .catch((e) => {
+        HttpErrorHandler(e, () => {
+          let errors = LaravelValidationParser(e.response.data.errors);
+          errors.forEach((error) => {
+            toast(error, { type: "error" });
+          });
+        });
+      });
+  };
+
   return (
-    <form className="d-flex my-3 bg-gray-500 p-2">
+    <form className="d-flex my-3 bg-gray-500 p-2" onSubmit={handleUnpark}>
       <ToastContainer />
       <input
         type="text"
