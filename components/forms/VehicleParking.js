56dd6483aaf64c78e6787cb95bfc0cd949704f82
Dylan Villaruel <thraxxdv@gmail.com>
2022-07-20 09:06:27 +0800

    style, build: added default value to vehicleparking dropdowns and formatted file

--- components/forms/VehicleParking.js
+++ components/forms/VehicleParking.js
@@ -4,7 +4,7 @@ import type { Element } from "react";
 import { parkVehicle } from "../../api/parking_space";
 
 import React from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function VehicleParking(): Element<"form"> {
   // Data
@@ -20,24 +20,39 @@ function VehicleParking(): Element<"form"> {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    parkVehicle(parseInt(selectedGate), parseInt(selectedType), timestamp, vehicleId)
+    parkVehicle(
+      parseInt(selectedGate),
+      parseInt(selectedType),
+      timestamp,
+      vehicleId
+    )
       .then((r) => console.log(r))
       .catch((e) => console.log(e));
   };
 
+  useEffect(() => {}, []);
+
   return (
     <form className="d-flex my-3" onSubmit={handleSubmit}>
-      <select name="gate" id="gate" className="form-select me-3" onChange={(e) => setSelectedGate(e.target.value)}>
-        <option value="1">test</option>
-        <option value="2">test</option>
+      <select
+        name="gate"
+        id="gate"
+        className="form-select me-3"
+        onChange={(e) => setSelectedGate(e.target.value)}
+        defaultValue={0}
+      >
+        <option value="0" selected disabled hidden>
+          Select Gate...
+        </option>
       </select>
       <select
         name="vehicle-type"
         id="vehicle-type"
         className="form-select me-3"
         onChange={(e) => setSelectedType(e.target.value)}
+        defaultValue={0}
       >
-      <option value="1">test vehicle type</option>
+        <option value="0" selected disabled hidden>Select Vehicle Type...</option>
         <option value="2">test vehicle type</option>
       </select>
       <input
